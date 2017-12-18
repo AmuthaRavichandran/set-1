d265a230368612e8a81ff156511bf684c4f5ba8e
AmuthaRavichandran <34593199+AmuthaRavichandran@users.noreply.github.com>
2017-12-18 21:56:33 +0530

    Update evod.py

--- evod.py
+++ evod.py
@@ -1,5 +1,5 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
+num=int(raw_input("enter a number: "))
+if(num%2==0):
+	print("even no")
 else:
-   print("{0} is Odd".format(num))
+	print("odd no")
