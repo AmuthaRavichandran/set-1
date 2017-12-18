858dba746397f6b139e316c2d2569178cd14ecd7
AmuthaRavichandran <34593199+AmuthaRavichandran@users.noreply.github.com>
2017-12-18 21:40:45 +0530

    Create vowelcorect.py

---
+++ vowelcorect.py
@@ -0,0 +1,6 @@
+ch=raw_input("enter any character")
+if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I'
+		   or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+	print(ch, "is a vowel.\n")
+else:
+	print(ch, "is not a vowel.\n")
