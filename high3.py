701f0befa52cd20f9fc532ddc078db1ec6fd8db0
AmuthaRavichandran <34593199+AmuthaRavichandran@users.noreply.github.com>
2018-06-11 13:39:57 +0530

    Update high3.py

--- high3.py
+++ high3.py
@@ -1,9 +1,6 @@
+print("enter three no")
 x=raw_input()
 y=raw_input()
 z=raw_input()
-if (x>y>z):
-    print ("x is large")
-elif (y>x>z):
-    print ("y is large")
-else:
-    print ("z is large")
+list=[x,y,z]
+print max(list)
